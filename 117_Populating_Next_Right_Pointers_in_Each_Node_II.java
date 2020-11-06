f36b0b8ae4e1ff787a7d3393ac18e4164a16f9f6
Mustafiz04 <kaifee000@gmail.com>
2020-11-06 23:09:34 +0530

    added question

---
+++ 117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        if( root == null ){
+            return null;
+        }
+        q.add(root);
+        BFS(q);
+        return root;
+    }
+    
+    public void BFS( Queue<Node> q ){
+        while( !q.isEmpty() ){
+            int size = q.size();
+            while( size > 0 ){
+                Node curr = q.poll();
+                if( size == 1 ){
+                    curr.next = null;
+                    size--;
+                }else{
+                    curr.next = q.peek();
+                    size--;
+                }
+                if( curr.left != null ){
+                    q.add(curr.left);
+                }
+                if( curr.right != null ){
+                    q.add(curr.right);
+                }
+            }
+            
+        }
+    }
+}
