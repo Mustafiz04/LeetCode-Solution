2e1246c848656c29023ae93abca44366bb94b7dd
Mustafiz04 <kaifee000@gmail.com>
2020-11-06 23:09:00 +0530

    added question

---
+++ 116_Populating_Next_Right_Pointers_in_Each_Node.java
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if( root != null ){
+            nextRight(root.left, root.right);
+        }
+        return root;
+    }
+    
+    public void nextRight( Node left, Node right ){
+        if( left != null ){
+            left.next = right;
+            nextRight( left.left, left.right );
+        }
+        if( right != null ){
+            nextRight( left.right, right.left );
+            nextRight( right.left, right.right );
+        }
+    }
+    
+}
